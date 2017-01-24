4a2302909ab4c96be4752190f7be5c6160699865
mraab <mraab@intershop.de>
2017-01-24 21:07:56 +0100

    add test for local properties configuration

--- src/test/groovy/com/intershop/gradle/versionrecommender/provider/PropertiesProviderTest.groovy
+++ src/test/groovy/com/intershop/gradle/versionrecommender/provider/PropertiesProviderTest.groovy
@@ -1,6 +1,7 @@
 package com.intershop.gradle.versionrecommender.provider
 
 import com.intershop.gradle.test.util.TestDir
+import com.intershop.gradle.versionrecommender.util.VersionExtension
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
@@ -36,7 +37,7 @@ class PropertiesProviderTest extends Specification {
         project = ProjectBuilder.builder().withName(canonicalName).withProjectDir(testProjectDir).build()
     }
 
-    def 'Check Properties Provider'() {
+    def 'Properties provider from file'() {
         when:
         ClassLoader classLoader = getClass().getClassLoader()
         File file = new File(classLoader.getResource('propertiestest/test.properties').getFile())
@@ -47,7 +48,7 @@ class PropertiesProviderTest extends Specification {
         provider.getVersion('com.intershop.business','businesscomp') == '2.4.5'
     }
 
-    def 'Check Properties Provider - properties only'() {
+    def 'Properties provider with properties only'() {
         when:
         PropertiesProvider provider = new PropertiesProvider('test', project, new File('empty.properties'))
 
@@ -58,7 +59,7 @@ class PropertiesProviderTest extends Specification {
         provider.getVersion('com.intershop.business','businesscomp') == '2.4.5'
     }
 
-    def 'Check Properties Provider - properties and file'() {
+    def 'Properties provider with properties and file'() {
         when:
         ClassLoader classLoader = getClass().getClassLoader()
         File file = new File(classLoader.getResource('propertiestest/test.properties').getFile())
@@ -73,7 +74,7 @@ class PropertiesProviderTest extends Specification {
         provider.getVersion('com.intershop.test','testcomp') == '1.1.2'
     }
 
-    def 'Check Properties Provider - properties with placeholders'() {
+    def 'Properties provider from file with placeholders'() {
         when:
         ClassLoader classLoader = getClass().getClassLoader()
         File file = new File(classLoader.getResource('propertiestest/test.properties').getFile())
@@ -88,7 +89,7 @@ class PropertiesProviderTest extends Specification {
         provider.getVersion('com.intershop.content','testcompb') == '1.1.1'
     }
 
-    def 'Check Properties Provider - properties with dependencies'() {
+    def 'Properties provider from file and properties with dependencies'() {
         when:
         PropertiesProvider provider = new PropertiesProvider('test', project, new File('empty.properties'))
 
@@ -100,5 +101,20 @@ class PropertiesProviderTest extends Specification {
         then:
         provider.getVersion('javax.validation','validation-api') == '1.1.0.Final'
         provider.getVersion('org.tmatesoft.svnkit','svnkit') == '1.8.14'
+    }
+
+    def 'Properties provider from file with LOCAL extension'() {
+        when:
+        ClassLoader classLoader = getClass().getClassLoader()
+        File file = new File(classLoader.getResource('propertiestest/test3.properties').getFile())
+
+        File confFile = new File(project.projectDir, 'version.properties')
+        confFile << file.getText()
+
+        PropertiesProvider provider = new PropertiesProvider('test', project, confFile)
+        provider.setVersionExtension(VersionExtension.LOCAL)
+
+        then:
+        provider.getVersion('com.intershop.platform', 'platformcomp') == '1.2.3-LOCAL'
     }
 }
